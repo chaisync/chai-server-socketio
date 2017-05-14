32fdc7381eebf24c0b35285b8ca2cbc5df9bc0b8
Yuancheng Li <liyuancheng0118@gmail.com>
2017-05-13 18:57:29 -0700

    change data display format, clean up code

--- public/js/controllers/serverCtrl.js
+++ public/js/controllers/serverCtrl.js
@@ -1,13 +1,28 @@
 var app = angular.module('myApp', []);
 
-app.controller("serverController", function($scope) {
+app.controller("serverController", function() {
     $.ajax({ url: "db.json", success: function(file_content) {
         var content = JSON.stringify(file_content);
         console.log(content);
+        var contentSplit = content.split(['},']);
+        console.log(contentSplit);
         var node = document.getElementById('jsonDisplay');
-        var newNode = document.createElement('code');
-        newNode.appendChild(document.createTextNode(content));
-        node.appendChild(newNode);
+        for (var i = 0; i < contentSplit.length; i++) {
+            var newNode = document.createElement('code');
+            var brNode = document.createElement('br');
+            var hNode = document.createElement('h4');
+            var number = i+1;
+            hNode.appendChild(document.createTextNode('Data #' + number));
+            if (i != contentSplit.length-1) {
+                newNode.appendChild(document.createTextNode(contentSplit[i] + '}'))
+            } else {
+                newNode.appendChild(document.createTextNode(contentSplit[i]))
+            }
+            console.log(i + ' + ' + contentSplit[i]);
+            node.appendChild(hNode);
+            node.appendChild(newNode);
+            node.appendChild(brNode);
+        }
     }
     });
 });
